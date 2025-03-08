ba6edec3966e1e633a7772c9dca455a141b2af97
Matthew Maurer <mmaurer@google.com>
2025-03-08 00:50:24 +0000

    Bind soc registration

--- drivers/soc/qcom/socinfo_rust.rs
+++ drivers/soc/qcom/socinfo_rust.rs
@@ -1,9 +1,11 @@
 //! Re-implementation of Qualcomm's Socinfo driver in Rust
+use kernel::c_str;
 use kernel::error::from_err_ptr;
-use kernel::prelude::*;
+use kernel::module_platform_driver;
 use kernel::platform;
 use kernel::platform::Device;
-use kernel::module_platform_driver;
+use kernel::prelude::*;
+use kernel::soc;
 
 module_platform_driver! {
     type: QcomSocInfo,
@@ -16,7 +18,11 @@ module_platform_driver! {
 // TODO module bindgen
 mod ffi {
     extern "C" {
-        pub(crate) fn qcom_smem_get(host: kernel::ffi::c_uint, item: kernel::ffi::c_uint, size: *mut usize) -> *mut kernel::ffi::c_void;
+        pub(crate) fn qcom_smem_get(
+            host: kernel::ffi::c_uint,
+            item: kernel::ffi::c_uint,
+            size: *mut usize,
+        ) -> *mut kernel::ffi::c_void;
     }
     pub(crate) const QCOM_SMEM_HOST_ANY: kernel::ffi::c_uint = kernel::ffi::c_uint::MAX;
     pub(crate) const SMEM_HW_SW_BUILD_ID: kernel::ffi::c_uint = 137;
@@ -30,13 +36,32 @@ fn qcom_smem_get(host: u32, item: u32) -> Result<&'static [u8]> {
     Ok(unsafe { core::slice::from_raw_parts(ptr as *const _, size) })
 }
 
-struct QcomSocInfo;
+#[pin_data]
+struct QcomSocInfo {
+    #[pin]
+    registration: soc::DeviceRegistration<&'static ()>,
+}
 
 impl platform::Driver for QcomSocInfo {
     type IdInfo = ();
     const OF_ID_TABLE: Option<kernel::of::IdTable<Self::IdInfo>> = None;
     fn probe(_dev: &mut Device, _id_info: Option<&Self::IdInfo>) -> Result<Pin<KBox<Self>>> {
         let _mem = qcom_smem_get(ffi::QCOM_SMEM_HOST_ANY, ffi::SMEM_HW_SW_BUILD_ID)?;
-        Ok(KBox::pin(Self, GFP_KERNEL)?)
+        let attrs = soc::DeviceAttribute {
+            family: Some(c_str!("Snapdragon").to_cstring()?),
+            machine: None,
+            revision: None,
+            serial_number: None,
+            soc_id: None,
+            data: &(),
+        };
+        Ok(KBox::pin_init(
+            try_pin_init!(
+                    Self {
+                        registration <- soc::DeviceRegistration::register(attrs),
+                    }
+            ),
+            GFP_KERNEL,
+        )?)
     }
 }
--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -73,6 +73,7 @@ pub mod revocable;
 pub mod security;
 pub mod seq_file;
 pub mod sizes;
+pub mod soc;
 mod static_assert;
 #[doc(hidden)]
 pub mod std_vendor;

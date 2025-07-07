878305d17052471c5f237901ffe6072fc3f6bcaa
Matthew Maurer <mmaurer@google.com>
2025-07-07 20:34:33 +0000

    rust: Extend DebugFS Sample to show dynamic values
    The expands on the sample code to show what dynamic management of
    debugfs objects might look like.
    
    Signed-off-by: Matthew Maurer <mmaurer@google.com>

--- samples/rust/rust_debugfs.rs
+++ samples/rust/rust_debugfs.rs
@@ -4,11 +4,15 @@
 
 //! Sample DebugFS exporting module
 
-use core::sync::atomic::{AtomicU32, Ordering};
+use core::sync::atomic::{AtomicU32, AtomicUsize, Ordering};
 use kernel::c_str;
 use kernel::debugfs::{Dir, File};
+use kernel::miscdevice::{MiscDevice, MiscDeviceOptions, MiscDeviceRegistration};
 use kernel::prelude::*;
-use kernel::sync::{new_mutex, Arc};
+use kernel::rbtree::RBTree;
+use kernel::sync::ArcBorrow;
+use kernel::sync::{new_mutex, Arc, Mutex};
+use kernel::uaccess::{UserSlice, UserSliceReader};
 
 module! {
     type: RustDebugFs,
@@ -25,6 +29,157 @@ struct RustDebugFs {
     _subdir: Dir,
     _file: File,
     _file_2: File,
+    _miscdev: Pin<KBox<MiscDeviceRegistration<DynDebugFs>>>,
+}
+
+use kernel::ioctl::{_IOC_SIZE, _IOW};
+
+#[repr(C)]
+struct PropUpdate {
+    id: usize,
+    atomic: usize,
+    mutex: usize,
+}
+
+const CREATE_DEV: u32 = _IOW::<usize>('|' as u32, 1);
+const REMOVE_DEV: u32 = _IOW::<usize>('|' as u32, 2);
+const SET_DEV_PROP: u32 = _IOW::<PropUpdate>('|' as u32, 3);
+
+#[vtable]
+impl MiscDevice for DynDebugFs {
+    type Ptr = Arc<Self>;
+    type Data = Arc<Self>;
+    fn open(_file: &kernel::fs::File, misc: &MiscDeviceRegistration<Self>) -> Result<Arc<Self>> {
+        Ok(misc.data().clone())
+    }
+    fn ioctl(
+        me: ArcBorrow<'_, Self>,
+        _file: &kernel::fs::File,
+        cmd: u32,
+        arg: usize,
+    ) -> Result<isize> {
+        let user_arg = UserSlice::new(arg, _IOC_SIZE(cmd)).reader();
+        match cmd {
+            CREATE_DEV => me.create_dev(user_arg),
+            REMOVE_DEV => me.remove_dev(user_arg),
+            SET_DEV_PROP => me.set_dev_prop(user_arg),
+            _ => Err(EINVAL),
+        }
+    }
+}
+
+#[pin_data]
+struct DynDebugFs {
+    dir: Dir,
+    #[pin]
+    devs: Mutex<RBTree<usize, Dev>>,
+}
+
+impl DynDebugFs {
+    fn create_dev(&self, mut arg: UserSliceReader) -> Result<isize> {
+        let dev_id: usize = arg.read()?;
+        let mut devs = self.devs.lock();
+        if devs.get(&dev_id).is_some() {
+            return Err(EEXIST);
+        }
+        devs.try_create_and_insert(dev_id, Dev::new(&self.dir)?, GFP_KERNEL)?;
+        Ok(0)
+    }
+    fn remove_dev(&self, mut arg: UserSliceReader) -> Result<isize> {
+        let dev_id: usize = arg.read()?;
+        let mut devs = self.devs.lock();
+        if devs.remove(&dev_id).is_none() {
+            return Err(ENOENT);
+        }
+        Ok(0)
+    }
+    fn set_dev_prop(&self, mut arg: UserSliceReader) -> Result<isize> {
+        let dev_id: usize = arg.read()?;
+        let prop_atomic: usize = arg.read()?;
+        let prop_mutex: usize = arg.read()?;
+        let devs = self.devs.lock();
+        let Some(dev) = devs.get(&dev_id) else {
+            return Err(ENOENT);
+        };
+
+        dev.data.atomic.store(prop_atomic, Ordering::Relaxed);
+        *dev.data.mutex.lock() = prop_mutex;
+
+        Ok(0)
+    }
+    fn new(base: &Dir) -> Result<Arc<Self>> {
+        Arc::pin_init(
+            pin_init! {
+                Self {
+                    dir: base.subdir(c_str!("dyn-debugfs")),
+                    devs <- new_mutex!(RBTree::new()),
+                }
+            },
+            GFP_KERNEL,
+        )
+    }
+}
+
+#[pin_data]
+struct DevData {
+    atomic: AtomicUsize,
+    #[pin]
+    mutex: Mutex<usize>,
+}
+
+struct DevDebug {
+    _atomic: File,
+    _mutex: File,
+    _combined: File,
+}
+
+struct Dev {
+    data: Arc<DevData>,
+    _debug: DevDebug,
+}
+
+impl Dev {
+    fn new(dir: &Dir) -> Result<Self> {
+        let data = Arc::pin_init(DevData::new(), GFP_KERNEL)?;
+        Ok(Self {
+            _debug: DevDebug::new(dir, &data),
+            data,
+        })
+    }
+}
+
+impl DevData {
+    fn new() -> impl PinInit<Self> {
+        pin_init! {
+            Self {
+                atomic: AtomicUsize::new(0),
+                mutex <- new_mutex!(0),
+            }
+        }
+    }
+}
+
+impl DevDebug {
+    fn new(dir: &Dir, data: &Arc<DevData>) -> Self {
+        Self {
+            _mutex: dir.fmt_file(c_str!("mutex"), data.clone(), &|x, w| {
+                write!(w, "mutex: {}", *x.mutex.lock())
+            }),
+            _atomic: dir.fmt_file(c_str!("atomic"), data.clone(), &|x, w| {
+                write!(w, "atomic: {}", x.atomic.load(Ordering::Relaxed))
+            }),
+            // This cannot be written in file-per-field style because it involves combining
+            // attributes available to two other debugfs files. We could still write it with
+            // PinInit, but we'd have to produce the `Arc<DevData>` to support it.
+            _combined: dir.fmt_file(c_str!("combined"), data.clone(), &|x, w| {
+                write!(
+                    w,
+                    "combined: {}",
+                    x.atomic.load(Ordering::Relaxed) + *x.mutex.lock()
+                )
+            }),
+        }
+    }
 }
 
 static EXAMPLE: AtomicU32 = AtomicU32::new(8);
@@ -67,10 +222,19 @@ impl kernel::Module for RustDebugFs {
         // Save the handles we want to preserve to our module object. They will be automatically
         // cleaned up when our module is unloaded.
         Ok(Self {
-            _debugfs: debugfs,
             _subdir: sub,
             _file: file,
             _file_2: file_2,
+            _miscdev: KBox::pin_init(
+                MiscDeviceRegistration::register(
+                    MiscDeviceOptions {
+                        name: c_str!("sample-debugfs-dynamic"),
+                    },
+                    DynDebugFs::new(&debugfs)?,
+                ),
+                GFP_KERNEL,
+            )?,
+            _debugfs: debugfs,
         })
     }
 }
